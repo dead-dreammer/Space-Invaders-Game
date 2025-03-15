e54b2129fc5ec3d2c94be3c55e6d05b8079c0c72
dead-dreammer <dalziel.sarkhot94@gmail.com>
2025-03-15 19:09:26 +0200

    create the foundation

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Space Invaders")
+
+running = True
+
+while True:
+
+    pygame.display.update()
