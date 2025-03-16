d72d8d40931282cf5426864b0a6d9e8dfe4667ba
dead-dreammer <dalziel.sarkhot94@gmail.com>
2025-03-16 07:50:29 +0200

    adding images

--- main.py
+++ main.py
@@ -5,8 +5,29 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invaders")
 
+icon = pygame.image.load("space.png")
+pygame.display.set_icon(icon)
+
+playerimg = pygame.image.load("space.png")
+px = 370
+py = 500
+
+new_width = 80
+new_height = 80
+image_resized = pygame.transform.scale(playerimg, (new_width, new_height))
+
+def player():
+    screen.blit(image_resized, (px, py))
+
 running = True
 
 while True:
+    screen.fill((0, 0, 0))
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    player()
     pygame.display.update()
