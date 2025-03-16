d7687a35466e48b9c847112dc22dfc6dc4751fd8
dead-dreammer <dalziel.sarkhot94@gmail.com>
2025-03-16 08:01:46 +0200

    movement mechanics

--- main.py
+++ main.py
@@ -16,18 +16,19 @@ new_width = 80
 new_height = 80
 image_resized = pygame.transform.scale(playerimg, (new_width, new_height))
 
-def player():
-    screen.blit(image_resized, (px, py))
+def player(x, y):
+    screen.blit(image_resized, (x, y))
 
 running = True
 
-while True:
+while running:
     screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    
-    player()
+    px -= 0.2 # causes the spaceship to move left 
+    # each time the while loop runs : 370 - 0.2 
+    player(px, py)
     pygame.display.update()
